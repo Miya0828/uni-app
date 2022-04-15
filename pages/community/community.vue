0e000a92a50bd36125906479e83c2cb041e2d4c1
Your Name <you@example.com>
2022-04-15 22:59:08 +0800

    擅长领域

--- pages/community/community.vue
+++ pages/community/community.vue
@@ -1,99 +1,203 @@
 <template>
-	<scroll-view scroll-y class="page">
-		<map
-			v-if="polyline[0].points.length > 0"
-			id="myMap"
-			:markers="markers"
-			:polyline="polyline"
-			:include-points="polyline[0].points"
-			:latitude="polyline[0].points[0].latitude"
-			:longitude="polyline[0].points[0].longitude"
-			style="width: 100%; height: 1000upx"
-		>
-			<!-- <cover-image class="cover-image" src="/static/logo.png"></cover-image> -->
-			<cover-view class="cover-image" @click="getSearch"><input type="text" placeholder="搜索位置" /></cover-view>
-		</map>
-	</scroll-view>
+	<view class="community-container">
+		<view class="community-container-list">
+			<view class="community-container-list-item" v-for="item in [1,2,3,4,5,6,7,8,9]" :key="item">
+				<view class="community-container-list-item-header">
+					<view class="community-container-list-item-header-left">
+						<image src="/static/logo.png" mode="aspectFit"></image>
+						<view class="community-container-list-item-header-name">
+							<view class="community-container-list-item-header-name-1">
+								安其拉
+							</view>
+							<view class="community-container-list-item-header-name-2">
+								1小时前
+							</view>
+						</view>
+					</view>
+					<view class="community-container-list-item-header-right">
+						<view class="community-container-list-item-header-right-button">
+							动态
+						</view>
+					</view>
+				</view>
+
+				<view class="community-container-list-item-content">
+					<view class="community-container-list-item-content-des">
+						最好的风景就是在路上…加油！
+					</view>
+					<view class="community-container-list-item-content-pic">
+						<image src="/static/logo.png" mode="aspectFit"></image>
+					</view>
+				</view>
+
+				<view class="community-container-list-item-feature">
+					<view class="community-container-list-item-feature-address">
+						<image src="/static/community/ic_location@3x.png" mode="aspectFit"></image>
+						<view>浙江省温州雁荡山·温州</view>
+					</view>
+					<view class="community-container-list-item-feature-time">
+						<text>拍摄于 2022.04.09</text>
+					</view>
+				</view>
+
+				<view class="community-container-list-item-action">
+					<view class="community-container-list-item-action-view">
+						<image src="/static/community/ic_eye@3x.png" mode="aspectFit"></image>
+						<text>1234</text>
+					</view>
+					<view class="community-container-list-item-action-view">
+						<image src="/static/community/ic_collect@3x.png" mode="aspectFit"></image>
+						<text>893</text>
+					</view>
+					<view class="community-container-list-item-action-view">
+						<image src="/static/community/ic_chat@3x.png" mode="aspectFit"></image>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
 </template>
 <script>
-export default {
-	data() {
-		return {
-			mapContext: null, //地图对象
-			startMove: false, //是否开始回放
-			nextPointIndex: 1, //下一个坐标点的索引
-			durationTime: 1000, //相邻两点动画持续时长默认1秒
-			//路线信息
-			polyline: [
-				{
-					width: 8,
-					points: [],
-					arrowLine: true,
-					color: '#3591FC'
-				}
-			],
-			//标记点(即移动标记物)
-			markers: [
-				{
-					id: 1,
-					width: 40,
-					height: 40,
-					latitude: 0,
-					longitude: 0,
-					iconPath: '/static/logo.png',
-					anchor: {
-						x: 0.5,
-						y: 1
-					}
-				}
-			]
-		};
-	},
-	mounted() {
-		this.getTrack(); //获取轨迹信息(只做演示，未进行远程请求)
-	},
-	methods: {
-		//模拟获取远程数据
-		getTrack() {
-			this.polyline[0].points = [
-				{ latitude: 39.997761, longitude: 116.478935 },
-				{ latitude: 39.997825, longitude: 116.478939 },
-				{ latitude: 39.998549, longitude: 116.478912 },
-				{ latitude: 39.998555, longitude: 116.478998 },
-				{ latitude: 39.998566, longitude: 116.479282 },
-				{ latitude: 39.998528, longitude: 116.479658 },
-				{ latitude: 39.998453, longitude: 116.480151 },
-				{ latitude: 39.998302, longitude: 116.480784 },
-				{ latitude: 39.998184, longitude: 116.481149 },
-				{ latitude: 39.997997, longitude: 116.481573 },
-				{ latitude: 39.997846, longitude: 116.481863 },
-				{ latitude: 39.997718, longitude: 116.482072 },
-				{ latitude: 39.997718, longitude: 116.482362 },
-				{ latitude: 39.998935, longitude: 116.483633 },
-				{ latitude: 39.998968, longitude: 116.48367 },
-				{ latitude: 39.999861, longitude: 116.484648 }
-			];
-			this.durationTime = Math.ceil(30000 / this.polyline[0].points.length); //默认播放全程使用30秒，计算相连两点动画时长
-			this.initMapData();
-		},
-		//设置地图
-		initMapData() {
-			this.initMarkers();
-			this.mapContext = uni.createMapContext('myMap', this);
-		},
-		//设置位置（从起点开始）
-		initMarkers() {
-			this.markers[0].latitude = this.polyline[0].points[0].latitude;
-			this.markers[0].longitude = this.polyline[0].points[0].longitude;
-		},
-		getSearch(e) {
-			console.log(e);
-		}
-	}
-};
+	export default {
+
+	};
 </script>
 
 <style lang="scss">
+	.community-container {
+		background-color: white;
+		padding: 0 32rpx;
+
+		.community-container-list {
+			.community-container-list-item+.community-container-list-item {
+				border-top: 2rpx solid #F8F8F8; //
+			}
+
+			.community-container-list-item {
+				padding: 36rpx 0;
+
+				.community-container-list-item-header {
+					display: flex;
+					justify-content: space-between;
+					margin-bottom: 20rpx;
+
+					.community-container-list-item-header-left {
+						display: flex;
+
+						image {
+							width: 72rpx;
+							height: 72rpx;
+						}
+
+						.community-container-list-item-header-name {
+							padding-left: 20rpx;
+
+							.community-container-list-item-header-name-1 {
+								font-size: 32rpx;
+								font-weight: 400;
+								color: #333333;
+								margin-bottom: 6rpx;
+							}
+
+							.community-container-list-item-header-name-2 {
+								font-size: 24rpx;
+								font-weight: 400;
+								color: #999999;
+							}
+						}
+					}
+
+					.community-container-list-item-header-right {
+						.community-container-list-item-header-right-button {
+							width: 120rpx;
+							height: 64rpx;
+							background: rgba(0, 137, 255, 0.1);
+							border-radius: 5rpx;
+							font-size: 28rpx;
+							font-weight: 400;
+							color: #0189FF;
+							display: flex;
+							justify-content: center;
+							align-items: center;
+						}
+					}
+				}
+
+				.community-container-list-item-content {
+					margin-bottom: 20rpx;
+
+					.community-container-list-item-content-des {
+						font-size: 32rpx;
+						font-weight: 400;
+						color: #333333;
+						margin-bottom: 20rpx;
+					}
+
+					.community-container-list-item-content-pic {}
+				}
+
+				.community-container-list-item-feature {
+					display: flex;
+					margin-bottom: 24rpx;
+
+
+					.community-container-list-item-feature-address {
+						padding: 12rpx 20rpx;
+						background: #F8F8F8;
+						border-radius: 8rpx;
+						font-size: 24rpx;
+						font-weight: 400;
+						color: #999999;
+						display: flex;
+						align-items: center;
+						margin-right: 20rpx;
+
+						image {
+							width: 24rpx;
+							height: 24rpx;
+							margin-right: 6rpx;
+						}
+
+					}
+
+					.community-container-list-item-feature-time {
+						padding: 12rpx 20rpx;
+						background: #F8F8F8;
+						border-radius: 8rpx;
+						font-size: 24rpx;
+						font-weight: 400;
+						color: #999999;
+						display: flex;
+					}
+				}
+
+				.community-container-list-item-action {
+					display: flex;
+					justify-content: flex-end;
+					font-size: 28rpx;
+					font-weight: 400;
+					color: #333333;
+
+					view+view {
+						margin-left: 60rpx;
+					}
+
+					.community-container-list-item-action-view {
+						display: flex;
+						align-items: center;
+
+						image {
+							width: 32rpx;
+							height: 32rpx;
+							margin-right: 4rpx;
+						}
+
+					}
+				}
+			}
+		}
+	}
+
 	page {
 		&::after {
 			content: ' ';
@@ -104,16 +208,5 @@ export default {
 			background-color: rgba(220, 220, 220, 0.5);
 			box-shadow: 0rpx -2rpx 2rpx 0rpx rgba(220, 220, 220, 0.5);
 		}
-	}
-.cover-image {
-		position: absolute;
-		left: 0;
-		top: 0;
-		right: 0;
-		bottom: 0;
-		margin: auto;
-		width: 96px;
-		height: 96px;
-		z-index: 1000000;
 	}
 </style>
---
+++ pages/mine/captainApplication/skilledIn.vue
@@ -0,0 +1,180 @@
+<template>
+	<view class="skilled-in-container">
+		<view class="skilled-in-container-header">
+			<uni-section title="控制清除/取消按钮" subTitle="使用 clearButton 属性设置清除按钮" type="line">
+				<uni-search-bar class="uni-mt-10" radius="5" placeholder="地名、地名首字母" clearButton="auto"
+					cancelButton="none" @confirm="search" />
+			</uni-section>
+		</view>
+		<view class="skilled-in-container-address">
+			<view class="skilled-in-container-address-des">
+				目前您的位置
+			</view>
+			<view class="skilled-in-container-address-content">
+				<view class="skilled-in-container-address-content-left">
+					<image src="/static/mine/ic_position@3x.png" mode="aspectFit"></image>
+					<text>永州市道县</text>
+				</view>
+				<view class="skilled-in-container-address-content-right">
+					重新定位
+				</view>
+			</view>
+		</view>
+
+		<view class="skilled-in-container-country">
+			<view class="skilled-in-container-country-item current">
+				国内
+			</view>
+			<view class="skilled-in-container-country-item">
+				国外
+			</view>
+		</view>
+		<view class="skilled-in-container-list">
+			<uni-collapse accordion @change="change">
+				<uni-collapse-item title="北京">
+					<view class="skilled-in-container-city-list">
+						<view class="skilled-in-container-list-city">
+							北京
+						</view>
+					</view>
+				</uni-collapse-item>
+				<uni-collapse-item title="江苏" v-for="(item,index) in [1,2,3,4,5,6,7,8,9,0,1,2,3]" :key="index">
+					<view class="skilled-in-container-city-list">
+						<view class="skilled-in-container-list-city">
+							徐州
+						</view>
+						<view class="skilled-in-container-list-city">
+							连云港
+						</view>
+						<view class="skilled-in-container-list-city">
+							南京
+						</view>
+						<view class="skilled-in-container-list-city">
+							宿迁
+						</view>
+						<view class="skilled-in-container-list-city">
+							常州
+						</view>
+						<view class="skilled-in-container-list-city">
+							苏州
+						</view>
+					</view>
+				</uni-collapse-item>
+			</uni-collapse>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				accordionVal: '1',
+			}
+		},
+		methods: {
+			change(e) {
+				console.log(e);
+			},
+			search(res) {
+				uni.showToast({
+					title: '搜索：' + res.value,
+					icon: 'none'
+				})
+			},
+			cancel(res) {
+				uni.showToast({
+					title: '点击取消，输入值为：' + res.value,
+					icon: 'none'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.skilled-in-container {
+		border-top: 1rpx solid rgba(0, 0, 0, 0.1);
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+		.skilled-in-container-header {}
+
+		.skilled-in-container-address {
+			.skilled-in-container-address-des {
+				background-color: #F8F8F8;
+				font-size: 24rpx;
+				font-weight: 400;
+				color: #999999;
+				padding: 20rpx 32rpx;
+			}
+
+			.skilled-in-container-address-content {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				padding: 24rpx 32rpx;
+
+				.skilled-in-container-address-content-left {
+					display: flex;
+					align-items: center;
+
+					image {
+						width: 30rpx;
+						height: 30rpx;
+
+						margin-right: 32rpx;
+					}
+				}
+
+				.skilled-in-container-address-content-right {
+					font-size: 24rpx;
+					font-weight: 400;
+					color: #008CFF;
+				}
+			}
+		}
+
+		.skilled-in-container-country {
+			background: #F8F8F8;
+			padding: 0 32rpx;
+			display: flex;
+			font-size: 32rpx;
+			font-weight: 500;
+			color: #333333;
+
+			.skilled-in-container-country-item {
+				padding: 20rpx 0;
+				margin-right: 60rpx;
+			}
+
+			.skilled-in-container-country-item.current {
+				color: #0089FF;
+				border-bottom: 5rpx solid #0089FF;
+			}
+		}
+
+		.skilled-in-container-list {
+			flex-grow: 1;
+			overflow: auto;
+			.skilled-in-container-city-list {
+				display: flex;
+				padding: 32rpx;
+				flex-wrap: wrap;
+
+				.skilled-in-container-list-city {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					border-radius: 8rpx;
+					border: 2rpx solid #999;
+					color: #999;
+					min-width: 100rpx;
+					height: 64rpx;
+					margin-right: 20rpx;
+					margin-bottom: 20rpx;
+				}
+			}
+		}
+	}
+</style>
